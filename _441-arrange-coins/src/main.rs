3e116292b21ccb9a7c01047032f9e7d2fd1cb53c
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2021-11-04 22:29:25 -0700

    Leetcode 441: Arrange coins (2)
    Solution using binary search

--- _441-arrange-coins/src/main.rs
+++ _441-arrange-coins/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let n = 5;
+    assert_eq!(arrange_coins_using_binary_search(n), 2);
 }
 
 fn arrange_coins(n: i32) -> i32 {
@@ -17,6 +18,29 @@ fn arrange_coins(n: i32) -> i32 {
         count += 1;
     }
     count - 1
+}
+
+fn arrange_coins_using_binary_search(n: i32) -> i32 {
+    let mut start = 1;
+    let mut end = n;
+    let mut mid = 0;
+    let mut curr_sum = 0;
+    while start <= end {
+        // To prevent the overflow, not used mid calc using (start + mid) / 2
+        mid = start + (end - start) / 2;
+        curr_sum = mid * (mid + 1) / 2;
+        if curr_sum == n {
+            return mid;
+        }
+
+        if n < curr_sum {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    end
 }
 
 #[cfg(test)]
