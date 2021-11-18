e42103b5769feeaaf3d4cf869a82e009ce5a272d
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2021-11-17 19:42:42 -0700

    Leetcode 448 solution
    Use the flipping in order to detect the missing numbers.

--- _448-find-disappeared-numbers/src/main.rs
+++ _448-find-disappeared-numbers/src/main.rs
@@ -1,21 +1,20 @@
-use std::collections::HashSet;
 fn main() {
     println!("Hello, world!");
 }
 
 fn find_disappeared_numbers(nums: Vec<i32>) -> Vec<i32> {
-    let mut res: Vec<i32> = vec![];
-    if nums.len() == 0 {
-        return res;
+    let mut nums = nums;
+    let n = nums.len();
+    for i in 0..n {
+        let index: usize = (nums[i].abs() as usize) - 1;
+        nums[index] = -nums[index].abs();
     }
 
-    let mut distinct: HashSet<i32> = HashSet::new();
-    for i in 0..nums.len() {
-        distinct.insert(nums[i]);
-    }
-    for i in 1..nums.len() as i32 + 1 {
-        if !distinct.contains(&i) {
-            res.push(i);
+    let mut res: Vec<i32> = vec![];
+    for i in 1..=n {
+        let index = i - 1;
+        if nums[index] > 0 {
+            res.push(i as i32);
         }
     }
     res
