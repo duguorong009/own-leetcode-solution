595cccf540a7d9c5d7599212a6c9ead7bf3f0d93
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2021-12-12 18:04:51 -0700

    Leetcode 1446. Consecutive characters(Easy)
    Clean code.

--- _1466-consecutive-charactres/src/main.rs
+++ _1466-consecutive-charactres/src/main.rs
@@ -2,22 +2,36 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn max_power(s: String) -> i32 {
-    let mut max_power = 1;
-    let s: Vec<&str> = s.split("").collect();
-    let mut power = 1;
-    let mut ch: &str = s[0];
-    for i in 1..s.len() {
-        if s[i] == ch {
-            power += 1;
-            max_power = max_power.max(power);
-        } else {
-            ch = s[i];
-            power = 1;
-        }
-    }
+// fn max_power(s: String) -> i32 {
+//     let mut max_power = 1;
+//     let s: Vec<&str> = s.split("").collect();
+//     let mut power = 1;
+//     let mut ch: &str = s[0];
+//     for i in 1..s.len() {
+//         if s[i] == ch {
+//             power += 1;
+//             max_power = max_power.max(power);
+//         } else {
+//             ch = s[i];
+//             power = 1;
+//         }
+//     }
 
-    max_power
+//     max_power
+// }
+
+fn max_power(s: String) -> i32 {
+    let mut prev: (char, usize) = (' ', 0);
+    let mut res = 0;
+    for c in s.chars() {
+        if c == prev.0 {
+            prev.1 += 1;
+        } else {
+            prev = (c, 1);
+        }
+        res = res.max(prev.1);
+    }
+    res as i32
 }
 
 #[cfg(test)]
