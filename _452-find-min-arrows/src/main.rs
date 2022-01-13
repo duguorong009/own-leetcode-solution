2039d2a05c15a895f8391f2690d474ec9b9cc8e1
Guorong <honeybee@Guorongs-MacBook-Pro.local>
2022-01-13 11:29:38 +0800

    Leetcode 452. Find the minimum arrows to burst balloons(medium)
    Add the sample solution from "rustgym".

--- _452-find-min-arrows/src/main.rs
+++ _452-find-min-arrows/src/main.rs
@@ -29,7 +29,22 @@ fn main() {
 // }
 
 fn find_min_arrow_shots(points: Vec<Vec<i32>>) -> i32 {
-    0
+    let mut points = points;
+    let n = points.len();
+    if n == 0 {
+        return 0;
+    }
+    points.sort_by_key(|p| p[1]);
+    let mut end = points[0][1];
+    let mut res = 1;
+    for i in 1..n {
+        if points[i][0] <= end {
+            continue;
+        }
+        end = points[i][1];
+        res += 1;
+    }
+    res
 }
 
 #[cfg(test)]
