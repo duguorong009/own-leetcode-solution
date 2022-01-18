1a95fc5c0488aa09b5e3776e584c650a9520c2f7
guorong <dudurong009@gmail.com>
2022-01-18 05:59:11 -0700

    Leetcode 605. Can place flowers(Easy)
    Add the `0` at the head and tail of the `flowerbed` array.

--- _605-can-place-flowers/src/main.rs
+++ _605-can-place-flowers/src/main.rs
@@ -4,27 +4,22 @@ fn main() {
 
 fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
     let mut flowerbed = flowerbed;
-    let n = flowerbed.len();
-    let mut sum = 0;
-    for i in 0..n {
-        if flowerbed[i] == 0 {
-            if i == 0 {
-                sum += 1;
-                flowerbed[i] = 1;
-            } else {
-                if flowerbed[i - 1] == 0 {
-                    sum += 1;
-                    flowerbed[i] = 1;
-                }
-            }
-        } else {
-            if i > 0 && flowerbed[i - 1] == 1 {
-                sum -= 1;
-            }
+    let mut n = n;
+
+    // Insert the `0` at the head and tail of the `flowerbed`.
+    flowerbed.insert(0, 0 );
+    flowerbed.push(0);  
+
+    let m = flowerbed.len();
+
+    for i in 1..m - 1 {
+        if flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0 {
+            flowerbed[i] = 1;
+            n -= 1;
         }
     }
 
-    sum >= n
+    n <= 0
 }
 
 #[cfg(test)]
