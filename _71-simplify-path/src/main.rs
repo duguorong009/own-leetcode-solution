ee43d07b779269dd006149a6cd46bc412efbea4c
guorong <dudurong009@gmail.com>
2022-02-11 23:01:24 -0700

    Leetcode 71. Simplify Path (medium)
    Split the input with terminator "/" & handle the cases.

---
+++ _71-simplify-path/src/main.rs
@@ -0,0 +1,52 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn simplify_path(path: String) -> String {
+    let mut stack: Vec<&str> = vec![];
+    let mut res = "".to_string();
+    for s in path.split_terminator('/') {
+        match s {
+            ".." => {
+                stack.pop();
+            }
+            "" | "." => {
+                continue;
+            }
+            _ => {
+                stack.push(s);
+            }
+        }
+    }
+    for s in stack {
+        res += "/";
+        res += s;
+    }
+    if res.is_empty() {
+        res += "/";
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let path = "/home/".to_string();
+        assert_eq!(simplify_path(path), "/home".to_string());
+    }
+
+    #[test]
+    fn test_2() {
+        let path = "/../".to_string();
+        assert_eq!(simplify_path(path), "/".to_string());
+    }
+
+    #[test]
+    fn test_3() {
+        let path = "/home//foo/".to_string();
+        assert_eq!(simplify_path(path), "/home/foo".to_string());
+    }
+}
