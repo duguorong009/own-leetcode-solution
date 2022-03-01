1c5797c59b753f7c65dbaf100524601d39bac6ac
guorong <dudurong009@gmail.com>
2022-02-28 20:30:18 -0700

    Leetcode 338: Counting bits(easy)
    Better solution - O(n) method

--- _338-counting-bits/src/main.rs
+++ _338-counting-bits/src/main.rs
@@ -2,10 +2,20 @@ fn main() {
     println!("Hello, world!");
 }
 
+// // O(n log n) method
+// fn count_bits(n: i32) -> Vec<i32> {
+//     let mut res: Vec<i32> = vec![];
+//     for i in 0..=n {
+//         res.push(i.count_ones() as i32);
+//     }
+//     res
+// }
+
+// O(n) method
 fn count_bits(n: i32) -> Vec<i32> {
-    let mut res: Vec<i32> = vec![];
-    for i in 0..=n {
-        res.push(i.count_ones() as i32);
+    let mut res: Vec<i32> = vec![0];
+    for i in 1..=n {
+        res.push(res[i as usize / 2] + (i & 1));
     }
     res
 }
