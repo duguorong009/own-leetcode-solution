9005ae88f9b8b30c1ad7de5340192acb3b1dbc6d
guorong <dudurong009@gmail.com>
2022-03-02 10:28:52 +0800

    leetcode 392. Is Subsequence(easy)
    Another possible solution

--- _392-is-subsequence/src/main.rs
+++ _392-is-subsequence/src/main.rs
@@ -2,24 +2,40 @@ fn main() {
     println!("Hello, world!");
 }
 
+// fn is_subsequence(s: String, t: String) -> bool {
+//     if s.len() == 0 {
+//         return true;
+//     }
+//     let s = s.chars().collect::<Vec<char>>();
+//     let t = t.chars().collect::<Vec<char>>();
+//     let mut s_ptr = 0 as usize;
+//     let mut t_ptr = 0 as usize;
+//     while t_ptr < t.len() {
+//         if s[s_ptr] == t[t_ptr] {
+//             s_ptr += 1;
+//             if s_ptr == s.len() {
+//                 break;
+//             }
+//         }
+//         t_ptr += 1;
+//     }
+//     s_ptr == s.len()
+// }
+
 fn is_subsequence(s: String, t: String) -> bool {
-    if s.len() == 0 {
-        return true;
-    }
-    let s = s.chars().collect::<Vec<char>>();
-    let t = t.chars().collect::<Vec<char>>();
-    let mut s_ptr = 0 as usize;
-    let mut t_ptr = 0 as usize;
-    while t_ptr < t.len() {
-        if s[s_ptr] == t[t_ptr] {
-            s_ptr += 1;
-            if s_ptr == s.len() {
-                break;
-            }
+    let mut i = 0;
+    let mut j = 0;
+    let n = s.len();
+    let m = t.len();
+    while i < n && j < m {
+        if s[i..=i] == t[j..=j] {
+            i += 1;
+            j += 1;
+        } else {
+            j += 1;
         }
-        t_ptr += 1;
     }
-    s_ptr == s.len()
+    i == n
 }
 
 #[cfg(test)]
