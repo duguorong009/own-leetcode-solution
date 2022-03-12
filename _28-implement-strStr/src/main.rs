2563bc268d8462720f8cab75f37b16dfaf09262c
guorong <dudurong009@gmail.com>
2022-03-11 20:44:48 -0700

    Leetcode 28. Implement "strStr()" (easy)
    Add an own solution

--- _28-implement-strStr/src/main.rs
+++ _28-implement-strStr/src/main.rs
@@ -2,15 +2,47 @@ fn main() {
     println!("Hello, world!");
 }
 
-// What should we return when needle is an empty string? This is a great question to ask during an interview.
-// For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+// // What should we return when needle is an empty string? This is a great question to ask during an interview.
+// // For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 
+// Super simple solution using built-in function
 fn str_str(haystack: String, needle: String) -> i32 {
     match haystack.find(&needle) {
         Some(v) => v as i32,
         None => -1,
     }
 }
+
+// // Own solution: Time-Limit Exceeded error
+// fn str_str(haystack: String, needle: String) -> i32 {
+//     if needle.is_empty() {
+//         return 0;
+//     }
+    
+//     let haystack: Vec<char> = haystack.chars().collect();
+//     let needle: Vec<char> = needle.chars().collect();
+//     let n = haystack.len();
+//     let m = needle.len();
+
+//     let mut i = 0;
+//     while i < n {
+//         let mut j = 0;
+//         if haystack[i] == needle[j] {
+//             while j < m && i + j < n {
+//                 println!("{}:{}", i, j);
+//                 if haystack[i + j] != needle[j] {
+//                     break;
+//                 }
+//                 j += 1;
+//             }
+//             if j == m {
+//                 return i as i32;
+//             }
+//         } 
+//         i += 1;
+//     }
+//     -1
+// }
 
 #[cfg(test)]
 mod tests {
